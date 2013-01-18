400455675ee118ce1e97d920d974fe9dd3db791d
Alex Reed <areed1@unbc.ca>
2013-01-17 18:39:00 -0800

    Initial Commit of Flapjacks Problem

---
+++ Flapjacks/src/Main.java
@@ -0,0 +1,54 @@
+/*
+ * Main.java
+ *  java program model for www.programming-challenges.com
+ */
+
+import java.io.*;
+import java.util.*;
+
+class Main implements Runnable{
+    static String ReadLn(int maxLength){  // utility function to read from stdin,
+        // Provided by Programming-challenges, edit for style only
+        byte line[] = new byte [maxLength];
+        int length = 0;
+        int input = -1;
+        try{
+            while (length < maxLength){//Read untill maxlength
+                input = System.in.read();
+                if ((input < 0) || (input == '\n')) break; //or untill end of line ninput
+                line [length++] += input;
+            }
+
+            if ((input < 0) && (length == 0)) return null;  // eof
+            return new String(line, 0, length);
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    public static void main(String args[])  // entry point from OS
+    {
+        Main myWork = new Main();  // Construct the bootloader
+        myWork.run();            // execute
+    }
+
+    public void run() {
+        new myStuff().run();
+    }
+}
+class myStuff implements Runnable{
+
+    public void run()
+    {
+
+
+    }
+
+    private void flip()
+    {
+
+
+
+    }
+    // You can insert more classes here if you want.
+}
