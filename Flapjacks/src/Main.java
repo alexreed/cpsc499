f840d6b380adb775358b449cbc6b9cd932f6fb6a
Alex Reed <areed1@unbc.ca>
2013-01-17 21:00:08 -0800

    Partially working solution to Flapjacks

--- Flapjacks/src/Main.java
+++ Flapjacks/src/Main.java
@@ -13,7 +13,7 @@ class Main implements Runnable{
         int length = 0;
         int input = -1;
         try{
-            while (length < maxLength){//Read untill maxlength
+            while (length < maxLength){//Read until maxlength
                 input = System.in.read();
                 if ((input < 0) || (input == '\n')) break; //or untill end of line ninput
                 line [length++] += input;
@@ -38,17 +38,99 @@ class Main implements Runnable{
 }
 class myStuff implements Runnable{
 
+    private ArrayList<Integer> stack = new ArrayList();
+
     public void run()
     {
+        while(true)
+        {
+            String input = Main.ReadLn(100000);
 
+            if(input.isEmpty())
+                break;
 
+            Scanner scanner = new Scanner(input);
+
+            while(scanner.hasNextInt())
+            {
+                int flapjack = scanner.nextInt();
+
+                stack.add(0,flapjack);
+
+                System.out.print(flapjack + " ");
+            }
+
+            System.out.println();
+            perfectStack();
+        }
     }
 
-    private void flip()
+    private void perfectStack()
     {
+        for(int flipPointer = 0; flipPointer < stack.size()-1; flipPointer++)
+        {
+            int greatestPointer = -1;
 
+            for(int greaterThanPointer = flipPointer+1; greaterThanPointer < stack.size();greaterThanPointer++)
+            {
+                if(stack.get(greaterThanPointer) > stack.get(flipPointer))
+                {
+                    if(greatestPointer == -1)
+                    {
+                         greatestPointer = greaterThanPointer;
+                    }
+                    else
+                    {
+                          if(stack.get(greaterThanPointer) > stack.get(greatestPointer))
+                          {
+                              greatestPointer = greaterThanPointer;
+                          }
+                    }
 
+                }
+            }
 
+            if(greatestPointer == stack.size()-1)    //require one flip
+            {
+                flip(flipPointer+1);
+
+                System.out.print(flipPointer+1 + " ");
+            }
+            else if(greatestPointer != -1) //require two flip
+            {
+                flip(greatestPointer+1);
+
+                System.out.print(greatestPointer+1 + " ");
+
+                flip(flipPointer+1);
+
+                System.out.print(flipPointer+1 + " ");
+            }
+        }
+
+        stack.clear();
+
+        System.out.println(0);
+    }
+
+    private void flip(int positionInStack)
+    {
+        positionInStack = positionInStack-1;
+
+        //2 5 7 6 4 8
+        List<Integer> miniStack = stack.subList(positionInStack,stack.size());
+
+        //7 6 4 8
+        Collections.reverse(miniStack);
+
+        for(int positionInMiniStack = 0; positionInMiniStack < miniStack.size(); positionInMiniStack++)
+        {
+             stack.set(positionInStack, miniStack.get(positionInMiniStack));
+
+            positionInStack++;
+        }
+
+        //System.out.println(stack.toString());
     }
     // You can insert more classes here if you want.
 }
